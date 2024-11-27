69522db0fc43544771238a439e92a0a192b937c6
Chumakov Aleksey <Alexeyse10@gmail.com>
2024-11-27 22:53:15 +0700

    Added calculator display and buttons

---
+++ Calculator/Calculator/Model/Model.swift
@@ -0,0 +1,30 @@
+//
+//  MainView.swift
+//  Calculator
+//
+//  Created by Алексей Чумаков on 27.11.2024.
+//
+
+import Foundation
+
+enum Buttons: String {
+    case zero = "0"
+    case one = "1"
+    case two = "2"
+    case three = "3"
+    case four = "4"
+    case five = "5"
+    case six = "6"
+    case seven = "7"
+    case eight = "8"
+    case nine = "9"
+    case plus = "+"
+    case minus = "-"
+    case multiple = "x"
+    case divide = "/"
+    case equal = "="
+    case decimal = "."
+    case percent = "%"
+    case negative = "+/-"
+    case clear = "AC"
+}
---
+++ Calculator/Calculator/View/MainView.swift
@@ -0,0 +1,62 @@
+//
+//  MainView.swift
+//  Calculator
+//
+//  Created by Алексей Чумаков on 27.11.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+
+    // MARK: Property
+    let buttonsArray: [[Buttons]] = [
+        [.clear, .negative, .percent, .divide],
+        [.seven, .eight, .nine, .multiple],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus],
+        [.zero, .decimal, .equal]
+    ]
+
+    var body: some View {
+
+        ZStack {
+            // MARK: Background
+            Color.black
+                .ignoresSafeArea()
+
+            VStack {
+
+                // MARK: Display
+                HStack {
+                    Spacer()
+                    Text("0")
+                        .foregroundStyle(.white)
+                        .font(.system(size: 90))
+                        .fontWeight(.light)
+                }
+
+                // MARK: Buttons
+                ForEach(buttonsArray, id: \.self) { row in
+                    HStack {
+                        ForEach(row, id: \.self) { item in
+                            Button {
+                                // action
+                            } label: {
+                                Text(item.rawValue)
+                                    .frame(width: 80, height: 80)
+                                    .foregroundStyle(Color.white)
+                                    .background(Color.orange)
+                                    .clipShape(Circle())
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MainView()
+}
